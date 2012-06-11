cb44983466e28c323ac83115bdd06bdb7d7d9e84
Mikołaj Dądela <mik01aj@o2.pl>
2012-06-11 09:50:05 +0200

    * pretty-printing alignments from the command line

--- toolkit/Alignment.py
+++ toolkit/Alignment.py
@@ -1,8 +1,18 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+This module contains the Alignment class, which is used to handle
+text alignments. You can also run this file it from the command line
+to pretty-print an alignment.
+"""
 
 import csv
 
 class Alignment:
+
+    """A class to hold an alignment. It doesn't know anything about the texts.
+    """
 
     def __init__(self, data):
         """Constructor.
@@ -91,5 +101,23 @@ class Alignment:
                 l1 = l1 if l1 != None else ""
                 l2 = l2 if l2 != None else ""
                 c = c if c != None else ""
-                print unicode("%-35s │ %-35s │ %s", 'utf-8') % (l1, l2, c)
+                s = unicode("%-35s │%-35s │%s", 'utf-8') % (l1, l2, c)
+                print s.encode('utf-8') # a workaround for `less`
             print
+
+
+if __name__ == '__main__':
+    import sys
+    import re
+    from Text import Text
+
+    [alignment_filename] = sys.argv[1:]
+
+    m = re.match(r'(.*)\.(..)-(..)\.(.*)$', alignment_filename)
+    a = Alignment.from_file(alignment_filename)
+    t1 = Text.from_file("%s.%s.txt" % (m.group(1), m.group(2)), lang=m.group(2))
+    t2 = Text.from_file("%s.%s.txt" % (m.group(1), m.group(3)), lang=m.group(3))
+    a.pretty_print(t1.as_sentences_flat(),
+                   t2.as_sentences_flat())
+    print "Total cost: " + str(sum(c for (_, _, c) in a.data))
+
