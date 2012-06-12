cd603d6a98f894127f2b36a9d1b4d4d3ff55b9a5
Mikołaj Dądela <mik01aj@o2.pl>
2012-06-12 23:04:04 +0200

    + added chasoslov (with some monolingual texts)

---
+++ texts/chasoslov/organize.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+#| 01polunosch.cu.txt   | Hora_02-Modlitwy_poranne.pl.txt | Meso.el.txt              | 1 |
+mv -iv Hora_02-Modlitwy_poranne.pl.txt 01polunosch.pl.txt
+mv -iv Meso.el.txt 01polunosch.el.txt
+#|                      |                                 | MesoSun.el.txt           |   |
+#|                      |                                 | MesoSat.el.txt           |   |
+#|                      |                                 | Orthros.el.txt           |   |
+#| 02utren.cu.txt       | Hora_03-Jutrznia.pl.txt         | OrthrosSun.el.txt        | 1 |
+mv -iv Hora_03-Jutrznia.pl.txt 02utren.pl.txt
+mv -iv OrthrosSun.el.txt 02utren.el.txt
+#|                      |                                 | Orthros40.el.txt         |   |
+#| 03chas1.cu.txt       | Hora_04-Pryma.pl.txt            | one.el.txt               | 1 |
+mv -iv Hora_04-Pryma.pl.txt 03chas1.pl.txt
+mv -iv one.el.txt 03chas1.el.txt
+#| 04chas3.cu.txt       | Hora_05-Tercja.pl.txt           | three.el.txt             | 1 |
+mv -iv Hora_05-Tercja.pl.txt 04chas3.pl.txt
+mv -iv three.el.txt 04chas3.el.txt
+#| 05chas6.cu.txt       | Hora_06-Seksta_i_typika.pl.txt  | six.el.txt               | 1 |
+mv -iv Hora_06-Seksta_i_typika.pl.txt 05chas6.pl.txt
+mv -iv six.el.txt 05chas6.el.txt
+#| 06izobrazit.cu.txt   |                                 |                          |   |
+#| 07panag.cu.txt       |                                 |                          |   |
+#| 08chas9.cu.txt       | Hora_07-Nona.pl.txt             | nine.el.txt              | 1 |
+mv -iv Hora_07-Nona.pl.txt 08chas9.pl.txt
+mv -iv nine.el.txt 08chas9.el.txt
+#| 09vechern.cu.txt     | Hora_08-Nieszpory.pl.txt        | Esperinos.el.txt         | 1 |
+mv -iv Hora_08-Nieszpory.pl.txt 09vechern.pl.txt
+mv -iv Esperinos.el.txt 09vechern.el.txt
+#| 10trapez.cu.txt      |                                 |                          |   |
+#| 11vpovech.cu.txt     | Hora_09-WPowieczerze.pl.txt     | Great.el.txt             | 1 |
+mv -iv Hora_09-WPowieczerze.pl.txt 11vpovech.pl.txt
+mv -iv Great.el.txt 11vpovech.el.txt
+#| 12mpovech.cu.txt     | Hora_09-MPowieczerze.pl.txt     | Apodeipnon.el.txt        | 1 |
+mv -iv Hora_09-MPowieczerze.pl.txt 12mpovech.pl.txt
+mv -iv Apodeipnon.el.txt 12mpovech.el.txt
+#| 13kafisma17.cu.txt   |                                 |                          |   |
+#| 14otpvoskr.cu.txt    | Hora_12-Tropariony.pl.txt       |                          | 1!|
+mv -iv Hora_12-Tropariony.pl.txt 14otpvoskr.pl.txt
+#| 15otpbogor.cu.txt    |                                 |                          |   |
+#| 16otpsedm.cu.txt     |                                 |                          |   |
+#| 17moleben.cu.txt     | Hora_19-Molebien.pl.txt         | paraklesis.el.txt        | 1 |
+mv -iv Hora_19-Molebien.pl.txt 17moleben.pl.txt
+mv -iv paraklesis.el.txt 17moleben.el.txt
+#| 18tropnepor.cu.txt   | Hora_20-Tropariony.pl.txt       |                          | 1!|
+mv -iv Hora_20-Tropariony.pl.txt 18tropnepor.pl.txt
+#| 19subbtropnep.cu.txt |                                 |                          |   |
+#|                      |                                 |                          |   |
+#|                      | Hora_10-Meka.pl.txt             |                          |   |
+#|                      | Hora_11-Pascha.pl.txt           |                          |   |
+#|                      |                                 |                          |   |
+#|                      | Hora_13-Swieta_wielkie.pl.txt   |                          |   |
+#|                      | Hora_14-Rok_liturgiczny.pl.txt  |                          |   |
+#|                      | Hora_15-Wspolne.pl.txt          |                          |   |
+#|                      | Hora_16-Triodion.pl.txt         |                          |   |
+#|                      | Hora_17-Rozeslania.pl.txt       |                          |   |
+#|                      | Hora_18-Prokimenony.pl.txt      |                          |   |
+#|                      | Hora_21-Poranne.pl.txt          |                          |   |
+#|                      | Hora_22-Wieczorne.pl.txt        |                          |   |
+#|                      |                                 |                          |   |
+#|                      |                                 | one40.el.txt             |   |
+#|                      |                                 | three40.el.txt           |   |
+#|                      |                                 | six40.el.txt             |   |
+#|                      |                                 | nine40.el.txt            |   |
+#|                      |                                 | example.el.txt           |   |
+#|                      |                                 | mesone.el.txt            |   |
+#|                      |                                 | mesthree.el.txt          |   |
+#|                      |                                 | messix.el.txt            |   |
+#|                      |                                 | mesnine.el.txt           |   |
+#|                      |                                 |                          |   |
+#|                      |                                 | Esperinos_Sunday.el.txt  |   |
+#|                      |                                 |                          |   |
+#|                      |                                 | Daily_Liturgy.el.txt     |   |
+#|                      |                                 | Sun_Liturgy.el.txt       |   |
+#|                      |                                 | Lit_with_Deacon.el.txt   |   |
+#|                      |                                 | Basil_Liturgy.el.txt     |   |
+#|                      |                                 | Pro.el.txt               |   |
+#|                      |                                 | StJames.el.txt           |   |
+#|                      |                                 |                          |   |
+#|                      |                                 | Akathist1to4.el.txt      |   |
+#|                      |                                 | Akathist5.el.txt         |   |
+#|                      |                                 | Staseis.el.txt           |   |
+#|                      |                                 | Typikon.el.txt           |   |
+#|                      |                                 | paraklesis_Great.el.txt  |   |
+#|                      |                                 | paraklesis_priest.el.txt |   |
