50991df3be40de9913c66dbd98f3999fd5d2ce57
Mikołaj Dądela <mik01aj@o2.pl>
2012-06-28 14:26:03 +0200

    minor bugfixes and optimizations

--- metaphone_groups.py
+++ metaphone_groups.py
@@ -7,20 +7,20 @@ from collections import defaultdict
 import sys
 import re
 
-from translit.metaphone import metaphone
+from translit.metaphone import metaphone, ignored_chars
 
 groups = defaultdict(lambda: set())
 
 if __name__ == '__main__':
+    ignored_chars_regex = re.compile("["+re.escape(ignored_chars)+"]", re.UNICODE)
     for line in sys.stdin:
         for word in line.split():
             word = unicode(word, 'utf-8')
-            word = re.sub("[=,.:;]", "", word.lower(), re.UNICODE)
-            for key in metaphone(word):
-                groups[key].add(word)
+            word = ignored_chars_regex.sub("", word.lower())
+            groups[metaphone(word)].add(word)
 
-    for m, ws in groups.items():
-        if len(ws) < 10:
+    for m, ws in sorted(groups.items()):
+        if len(ws) < 5:
             continue
         print (m + "\t" + " ".join(ws)).encode('utf-8')
 
