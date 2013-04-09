f5ff643542cd201c38216cdba35e51158e8b18f7
Mikołaj Dądela <mik01aj@o2.pl>
2013-04-09 22:39:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='corthus',
+    version='0.1dev',
+    packages=['corthus',],
+    license='Apache License Version 2.0',
+    long_description=open('README.md').read(),
+)
