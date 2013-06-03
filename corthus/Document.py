ddd97e01e77bfabef80071a19ed470b98bc33e0e
Mikołaj Dądela <mik01aj@o2.pl>
2013-06-03 21:44:48 +0200

    Added a lot new (non-working) tests.

--- corthus/Document.py
+++ corthus/Document.py
@@ -4,39 +4,38 @@ import itertools
 class Document(object):
     def __init__(self, data=None):
         if not data:
-            self.first_row = None
-            self.last_row = None
+            self._rows = []
         self._length = 0
 
-    class Row(dict):
+    class _Row(dict):
         def __init__(self, document, row_data, prev_row=None, next_row=None):
             dict.__init__(self, row_data)
             self.document = document
             self.dict = dict
-            self.prev_row = prev_row
-            self.next_row = next_row
+            self._prev = prev_row
+            self._next = next_row
+
+        def remove(self):
+            if self._prev:
+                self._prev._next = self._next
+            if self._next:
+                self._next._prev = self._prev
+            self.document._rows.remove(self)
 
     def add_row(self, row_data):
         self._length += 1
-        new_row = Document.Row(self, row_data, prev_row=self.last_row)
-        if not self.first_row:
-            self.first_row = self.last_row = new_row
-        else:
-            self.last_row.next_row = new_row
-            self.last_row = new_row
+        new_row = Document._Row(self, row_data)
+        if self._rows:
+            self._rows[-1]._next = new_row
+            new_row._prev = self._rows[-1]
+        self._rows.append(new_row)
         return new_row
 
     def __len__(self):
-        return self._length
+        return len(self._rows)
 
     def __getitem__(self, n):
-        try:
-            return next(itertools.islice(self, n, None))
-        except (StopIteration, ValueError):
-            raise IndexError(n)
+        return self._rows[n]
 
     def __iter__(self):
-        row = self.first_row
-        while row:
-            yield row
-            row = row.next_row
+        return iter(self._rows)
--- corthus/Document_test.py
+++ corthus/Document_test.py
@@ -8,38 +8,110 @@ class DocumentTest(unittest.TestCase):
 
     def test_len(self):
         self.assertEqual(0, len(self.d))
-        self.d.add_row({'a': 'aaa'})
+        self.d.add_row({'a': ['aaa']})
         self.assertEqual(1, len(self.d))
-        self.d.add_row({'b': 'bbb'})
+        self.d.add_row({'b': ['bbb']})
         self.assertEqual(2, len(self.d))
 
     def test_iter(self):
         self.assertEqual([], list(self.d))
-        a = self.d.add_row({'a': 'aaa'})
+        a = self.d.add_row({'a': ['aaa']})
         self.assertEqual([a], list(self.d))
-        b = self.d.add_row({'b': 'bbb'})
+        b = self.d.add_row({'b': ['bbb']})
         self.assertEqual([a, b], list(self.d))
 
     def test_getitem(self):
         self.assertRaises(IndexError, self.d.__getitem__, 0)
-        a = self.d.add_row({'a': 'aaa'})
+        a = self.d.add_row({'a': ['aaa']})
         self.assertEqual(a, self.d[0])
         self.assertRaises(IndexError, self.d.__getitem__, 1)
-        b = self.d.add_row({'b': 'bbb'})
+        b = self.d.add_row({'b': ['bbb']})
         self.assertEqual(a, self.d[0])
         self.assertEqual(b, self.d[1])
         self.assertRaises(IndexError, self.d.__getitem__, 2)
 
     def test_getitem_invalid(self):
         self.assertRaises(IndexError, self.d.__getitem__, -1)
+        self.assertRaises(IndexError, self.d.__getitem__, 0)
         self.assertRaises(IndexError, self.d.__getitem__, 1000)
-        self.d.add_row({'a': 'aaa'})
-        self.assertRaises(IndexError, self.d.__getitem__, -1)
+        self.d.add_row({'a': ['aaa']})
         self.assertRaises(IndexError, self.d.__getitem__, 1000)
 
     def test_remove(self):
-        self.d.add_row({'a': 'aaa'})
+        self.d.add_row({'a': ['aaa']})
         self.d[0].remove()
+
+    def test_link(self):
+        self.d.add_row({'a': ['a']})
+        self.d.add_row({'a': ['aa']})
+        self.d.add_row({'a': ['aaa']})
+        d2 = Document()
+        d2.add_row({'b': ['b']})
+        d2.add_row({'b': ['bb']})
+        joined = Document.join(self.d, d2)
+        self.assertEqual(list(joined),
+                         [
+                             {'a': ['a'], 'b': ['b']},
+                             {'a': ['aa'], 'b': ['bb']},
+                             {'a': ['aaa']},
+                         ])
+
+    def test_split(self):
+        row = self.d.add_row({
+            'a': ['a', 'aa', 'aaa'],
+            'b': ['b', 'bb', 'bbb'],
+        })
+        row.split({'a': 1, 'b': 2})
+        self.assertEqual(list(self.d),
+                         [
+                             {'a': ['a'], 'b': ['b', 'bb']},
+                             {'a': ['aa', 'aaa'], 'b': ['bbb']},
+                         ])
+
+    def test_merge(self):
+        self.d.add_row({'a': ['a'], 'b': ['b']})
+        self.d.add_row({'a': ['aa'], 'b': ['bb']})
+        self.d[0].merge_next()
+        self.assertEqual(list(self.d),
+                         [{'a': ['a', 'aa'], 'b': ['b', 'bb']}])
+
+    def test_align(self):
+        row = self.d.add_row({
+            'a': ['a', 'aa', 'aaa'],
+            'b': ['b', 'bb', 'bbb'],
+        })
+        row.align()
+        self.assertEqual(list(self.d),
+                         [
+                             {'a': ['a'], 'b': ['b']},
+                             {'a': ['aa'], 'b': ['bb']},
+                             {'a': ['aaa'], 'b': ['bbb']},
+                         ])
+
+
+class DocumentImportExportTest(unittest.TestCase):
+    def setUp(self):
+        self.d = Document()
+        self.d.add_row({
+            'a': ['a', 'aa', 'aaa'],
+            'b': ['b', 'bb', 'bbb'],
+        })
+        self.d.add_row({
+            'a': ['aaaa'],
+            'c': ['c'],
+        })
+        self.d.add_row({
+            'c': ['cc'],
+        })
+
+    def test_constructor(self):
+        d2 = Document(self.d)
+        self.assertEqual(list(self.d), list(d2))
+
+    def test_json(self):
+        d2 = Document.from_json(self.d.to_json())
+        self.assertEqual(list(self.d), list(d2))
+
 
 if __name__ == '__main__':
     unittest.main()
