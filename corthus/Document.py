f0c7787c4a4504ecbf63df5c7b0e4c2bb467b8ad
Mikołaj Dądela <mik01aj@o2.pl>
2013-06-04 09:02:43 +0200

    Most tests pass (only json, merge and align doesn't).

--- corthus/Document.py
+++ corthus/Document.py
@@ -1,41 +1,85 @@
 import itertools
+import copy
 
 
 class Document(object):
     def __init__(self, data=None):
         if not data:
             self._rows = []
-        self._length = 0
+        else:
+            assert isinstance(data, Document)
+            self._rows = copy.deepcopy(data._rows)
 
-    class _Row(dict):
-        def __init__(self, document, row_data, prev_row=None, next_row=None):
-            dict.__init__(self, row_data)
-            self.document = document
-            self.dict = dict
-            self._prev = prev_row
-            self._next = next_row
+    def append(self, row_data):
+        return self.insert(len(self._rows), row_data)
 
-        def remove(self):
-            if self._prev:
-                self._prev._next = self._next
-            if self._next:
-                self._next._prev = self._prev
-            self.document._rows.remove(self)
-
-    def add_row(self, row_data):
-        self._length += 1
-        new_row = Document._Row(self, row_data)
-        if self._rows:
-            self._rows[-1]._next = new_row
-            new_row._prev = self._rows[-1]
-        self._rows.append(new_row)
+    def insert(self, index, row_data):
+        """
+        Insert a row before `index`.
+        """
+        assert 0 <= index <= len(self._rows)
+        new_row = _Row(self, row_data)
+        if index > 0:
+            self._rows[index - 1]._next = new_row
+            new_row._prev = self._rows[index - 1]
+        if index < len(self._rows):
+            self._rows[index]._prev = new_row
+            new_row._next = self._rows[index]
+        new_row._index = index
+        self._rows.insert(index, new_row)
+        for i in xrange(index, len(self._rows)): # this makes the whole operation O(n)
+            self._rows[i]._index = i
         return new_row
+
+    @classmethod
+    def zip(cls, *documents):
+        new_doc = Document()
+        for row_dicts in itertools.izip_longest(*documents):
+            new_row_data = {}
+            for d in row_dicts:
+                if d:
+                    new_row_data.update(d)
+            new_doc.append(new_row_data)
+        return new_doc
 
     def __len__(self):
         return len(self._rows)
 
     def __getitem__(self, n):
-        return self._rows[n]
+        r = self._rows[n]
+        assert r._index == n
+        return r
 
     def __iter__(self):
         return iter(self._rows)
+
+
+class _Row(dict):
+    def __init__(self, document, row_data, index=None, prev_row=None, next_row=None):
+        dict.__init__(self, row_data)
+        self.document = document
+        self._prev = prev_row
+        self._next = next_row
+        self._index = index
+
+    def remove(self):
+        if self._prev:
+            self._prev._next = self._next
+        if self._next:
+            self._next._prev = self._prev
+        self.document._rows.remove(self)
+
+    def add_before(self, new_row):
+        self.document.insert(self._index, new_row)
+
+    def add_after(self, new_row):
+        self.document.insert(self._index + 1, new_row)
+
+    def split(self, indices):
+        assert self.keys() == indices.keys()
+        new_row_data = dict()
+        for key in self:
+            new_row_data[key] = self[key][indices[key]:]
+            self[key] = self[key][:indices[key]]
+        self.add_after(new_row_data)
+
--- corthus/Document_test.py
+++ corthus/Document_test.py
@@ -8,24 +8,24 @@ class DocumentTest(unittest.TestCase):
 
     def test_len(self):
         self.assertEqual(0, len(self.d))
-        self.d.add_row({'a': ['aaa']})
+        self.d.append({'a': ['aaa']})
         self.assertEqual(1, len(self.d))
-        self.d.add_row({'b': ['bbb']})
+        self.d.append({'b': ['bbb']})
         self.assertEqual(2, len(self.d))
 
     def test_iter(self):
         self.assertEqual([], list(self.d))
-        a = self.d.add_row({'a': ['aaa']})
+        a = self.d.append({'a': ['aaa']})
         self.assertEqual([a], list(self.d))
-        b = self.d.add_row({'b': ['bbb']})
+        b = self.d.append({'b': ['bbb']})
         self.assertEqual([a, b], list(self.d))
 
     def test_getitem(self):
         self.assertRaises(IndexError, self.d.__getitem__, 0)
-        a = self.d.add_row({'a': ['aaa']})
+        a = self.d.append({'a': ['aaa']})
         self.assertEqual(a, self.d[0])
         self.assertRaises(IndexError, self.d.__getitem__, 1)
-        b = self.d.add_row({'b': ['bbb']})
+        b = self.d.append({'b': ['bbb']})
         self.assertEqual(a, self.d[0])
         self.assertEqual(b, self.d[1])
         self.assertRaises(IndexError, self.d.__getitem__, 2)
@@ -34,21 +34,21 @@ class DocumentTest(unittest.TestCase):
         self.assertRaises(IndexError, self.d.__getitem__, -1)
         self.assertRaises(IndexError, self.d.__getitem__, 0)
         self.assertRaises(IndexError, self.d.__getitem__, 1000)
-        self.d.add_row({'a': ['aaa']})
+        self.d.append({'a': ['aaa']})
         self.assertRaises(IndexError, self.d.__getitem__, 1000)
 
     def test_remove(self):
-        self.d.add_row({'a': ['aaa']})
+        self.d.append({'a': ['aaa']})
         self.d[0].remove()
 
-    def test_link(self):
-        self.d.add_row({'a': ['a']})
-        self.d.add_row({'a': ['aa']})
-        self.d.add_row({'a': ['aaa']})
+    def test_zip(self):
+        self.d.append({'a': ['a']})
+        self.d.append({'a': ['aa']})
+        self.d.append({'a': ['aaa']})
         d2 = Document()
-        d2.add_row({'b': ['b']})
-        d2.add_row({'b': ['bb']})
-        joined = Document.join(self.d, d2)
+        d2.append({'b': ['b']})
+        d2.append({'b': ['bb']})
+        joined = Document.zip(self.d, d2)
         self.assertEqual(list(joined),
                          [
                              {'a': ['a'], 'b': ['b']},
@@ -57,7 +57,7 @@ class DocumentTest(unittest.TestCase):
                          ])
 
     def test_split(self):
-        row = self.d.add_row({
+        row = self.d.append({
             'a': ['a', 'aa', 'aaa'],
             'b': ['b', 'bb', 'bbb'],
         })
@@ -69,14 +69,14 @@ class DocumentTest(unittest.TestCase):
                          ])
 
     def test_merge(self):
-        self.d.add_row({'a': ['a'], 'b': ['b']})
-        self.d.add_row({'a': ['aa'], 'b': ['bb']})
+        self.d.append({'a': ['a'], 'b': ['b']})
+        self.d.append({'a': ['aa'], 'b': ['bb']})
         self.d[0].merge_next()
         self.assertEqual(list(self.d),
                          [{'a': ['a', 'aa'], 'b': ['b', 'bb']}])
 
     def test_align(self):
-        row = self.d.add_row({
+        row = self.d.append({
             'a': ['a', 'aa', 'aaa'],
             'b': ['b', 'bb', 'bbb'],
         })
@@ -92,15 +92,15 @@ class DocumentTest(unittest.TestCase):
 class DocumentImportExportTest(unittest.TestCase):
     def setUp(self):
         self.d = Document()
-        self.d.add_row({
+        self.d.append({
             'a': ['a', 'aa', 'aaa'],
             'b': ['b', 'bb', 'bbb'],
         })
-        self.d.add_row({
+        self.d.append({
             'a': ['aaaa'],
             'c': ['c'],
         })
-        self.d.add_row({
+        self.d.append({
             'c': ['cc'],
         })
 
